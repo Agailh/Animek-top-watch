98b162b19c5fa6880843fa5b7bcad36a8fd3f72c
Agailh <agailham86@gmail.com>
2022-09-28 15:37:32 +0700

    fetch api data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <Hero />
+  </div>
 </template>
 
 <script>
+import Hero from "../components/Hero.vue";
+import axios from "axios";
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      Animek: [],
+    };
+  },
+  async fetch() {
+    await this.getTopAnime();
+  },
+
+  methods: {
+    async getTopAnime() {
+      const data = axios.get("https://api.jikan.moe/v4/top/anime");
+      const result = await data;
+      result.data.data.forEach((anime) => {
+        this.Animek.push(anime);
+      });
+      console.log(this.Animek);
+    },
+  },
+
+  name: "IndexPage",
+  components: { Hero },
+};
 </script>
